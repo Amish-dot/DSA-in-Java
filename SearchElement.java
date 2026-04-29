095701c69b163af8d2d7cbe27db790afc93dcbc3
Amish Paliwal <amishpaliwal80@gmail.com>
2026-04-30 00:33:50 +0530

    Add files via upload

---
+++ SearchElement.java
@@ -0,0 +1,20 @@
+ public class SearchElement {
+    public static void main(String[] args) {
+        int[] arr = {5, 10, 15, 20, 25};
+        int target = 15;
+        boolean found = false;
+
+        for (int num : arr) {
+            if (num == target) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Element found");
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
